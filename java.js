80c221a784359eb7109e7157a698acac9714c6d2
miguel salazar <migueljoel80@gmail.com>
2022-09-27 08:46:39 -0500

    se crearon los archivos

---
+++ java.js
@@ -0,0 +1,39 @@
+dineroCofla = prompt("¿cuanto dinero tiene cofla?");
+dineroRoberto = prompt("¿cuanto dinero tiene Roberto?");
+dineroPedro = prompt("¿cuanto dinero tiene Pedro?");
+if (dineroCofla >= 0.6 && dineroCofla <1){
+    alert("cofla, comprate el helado de agua")}
+else if (dineroCofla >= 1 && dineroCofla <1.6){
+    alert("cofla, comprate el helado de crema fresa")}
+else if (dineroCofla >= 1.6 && dineroCofla <1.7){
+    alert("cofla, comprate el helado de crema choco")}
+else if (dineroCofla >= 1.7 && dineroCofla <1.8){
+    alert("cofla, comprate el helado de crema naranja")}
+else if (dineroCofla >= 1.8 && dineroCofla <2.9){
+    alert("cofla, comprate el helado de crema coco")}
+else if (dineroCofla >= 2,9){
+    alert("cofla, no te alcanza")}
+if (dineroRoberto >= 0.6 && dineroRoberto <1){
+    alert("Roberto comprate el helado de agua")}
+else if (dineroRoberto >= 1 && dineroRoberto <1.6){
+    alert("Roberto, comprate el helado de crema fresa")}
+else if (dineroRoberto >= 1.6 && dineroRoberto <1.7){
+    alert("Roberto, comprate el helado de crema choco")}
+else if (dineroRoberto >= 1.7 && dineroRoberto <1.8){
+    alert("Roberto, comprate el helado de crema naranja")}
+else if (dineroRoberto >= 1.8 && dineroRoberto <2.9){
+    alert("Roberto, comprate el helado de crema coco")}
+else if (dineroRoberto >= 2,9){
+    alert("Roberto, no te alcanza")}
+if (dineroPedro >= 0.6 && dineroPedro <1){
+    alert("Pedro, comprate el helado de agua")}
+else if (dineroPedro >= 1 && dineroPedro <1.6){
+    alert("Pedro, comprate el helado de crema fresa")}
+else if (dineroPedro >= 1.6 && dineroPedro <1.7){
+    alert("Pedro, comprate el helado de crema choco")}
+else if (dineroPedro >= 1.7 && dineroPedro <1.8){
+    alert("Pedro, comprate el helado de crema naranja")}
+else if (dineroPedro >= 1.8 && dineroPedro <2.9){
+    alert("Pedro, comprate el helado de crema coco")}
+else if (dineroPedro >= 2,9){
+    alert("Pedro, no te alcanza")}
